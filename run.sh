4afca62e687b464b9964e04d6832522a8e2c5ac4
Alex Popiel <tapopiel@gmail.com>
2026-03-21 17:34:04 -0700

    Fix run.sh because sbt is verbose

--- run.sh
+++ run.sh
@@ -2,7 +2,9 @@
 set -e
 
 # Get runtime classpath from sbt (fail if empty)
-CLASSPATH=$(sbt --batch 'export runtime:fullClasspath' 2>/dev/null)
+CLASSPATH=$(sbt --batch 'export runtime:fullClasspath' 2>/dev/null | tail -1 | sed 's/\\/\//g')
+# CLASSPATH=$(sbt --batch 'export runtime:fullClasspath' 2>/dev/null)
+echo "Using CLASSPATH=$CLASSPATH"
 
 if [ -z "$CLASSPATH" ]; then
     echo "Error: sbt returned empty classpath" >&2
